a0cffed9afd2b5657ca2c594c2f8e7f2c1654969
quanxianshuai <1570342684@qq.com>
2021-04-10 20:04:38 +0800

    一次定位Demo first commit

---
+++ MTLocationOnce/MTLocationOnce/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  MTLocationOnce
+//
+//  Created by 全先帅 on 2021/4/10.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ MTLocationOnce/MTLocationOnce/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  MTLocationOnce
+//
+//  Created by 全先帅 on 2021/4/10.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
