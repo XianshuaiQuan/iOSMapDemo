58e60f1b90dd0ae7079e78c428f0f95bc8ba6bac
quanxianshuai <1570342684@qq.com>
2021-04-20 20:46:38 +0800

    自定义添加大头针完成

--- MTMapViewDemo/MTMapViewDemo/ViewController.m
+++ MTMapViewDemo/MTMapViewDemo/ViewController.m
@@ -26,6 +26,7 @@
     
     [self.view addSubview:self.mapView];
     [self showUserLocation];
+    [self addMKPointAnnotation];
 }
 
 #pragma mark - 显示用户位置
@@ -39,6 +40,32 @@
     
     //显示用户位置时设置默认显示范围
     [self.mapView setRegion:MKCoordinateRegionMake(self.mapView.userLocation.location.coordinate, MKCoordinateSpanMake(0.024, 0.017)) animated:YES];
+}
+
+#pragma mark - 添加大头针
+- (void)addMKPointAnnotation {
+    //添加系统定义的大头针
+    MKPointAnnotation *pointAnnotation = [[MKPointAnnotation alloc] initWithCoordinate:self.mapView.userLocation.location.coordinate title:@"大头针" subtitle:@"MKPointAnnotation"];
+    [self.mapView addAnnotation:pointAnnotation];
+}
+
+#pragma mark - touchBegin
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    static int annotationNum = 0;
+    if (annotationNum >= 3) {
+        return;
+    }
+    //获取点击的点
+    CGPoint touchPoint = [[touches anyObject] locationInView:self.mapView];
+    //将点转化为经纬度
+    CLLocationCoordinate2D coordination = [self.mapView convertPoint:touchPoint toCoordinateFromView:self.mapView];
+    //添加用户自定义大头针
+    MMCAnnotationModel *annotation = [[MMCAnnotationModel alloc] init];
+    annotation.coordinate = coordination;
+    annotation.title = @"aa";
+    annotation.subtitle = @"bb";
+    [self.mapView addAnnotation:annotation];
+    annotationNum ++;
 }
 
 #pragma mark - mapViewDelegate
@@ -57,47 +84,53 @@
     [self.mapView setRegion:MKCoordinateRegionMake(mapView.userLocation.location.coordinate, mapView.region.span) animated:YES];
 }
 
-//添加大头针视图
-- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
-    //判断是系统自带的还是用户自定义的(MKUserrLocation 是系统大头针模型)
-    if ([annotation isKindOfClass:[MKUserLocation class]]) {
-        //如果是用户自定义，则返回nil，表示采用系统默认大头针模型
-        return nil;
+//在添加大头针时调用，且在图像出现之前调用
+- (void)mapView:(MKMapView *)mapView didAddAnnotationViews:(NSArray<MKAnnotationView *> *)views {
+    for (MKAnnotationView *annotationView in views) {
+        if ([annotationView.annotation isKindOfClass:[MKUserLocation class]]) {
+            return;
+        }
+        //记录原位置
+        CGRect frame = annotationView.frame;
+        //更改view的y值
+        annotationView.frame = CGRectMake(frame.origin.x, 0, frame.size.width, frame.size.height);
+        //设置动画
+        [UIView animateWithDuration:0.5 animations:^{
+            annotationView.frame = frame;
+        }];
     }
-    
-    
-    static NSString *identifier = @"MKAnnotationViewIdentifier";
-    MKAnnotationView *annotationView = [mapView dequeueReusableAnnotationViewWithIdentifier:identifier];
-    if (!annotationView) {
-        //用户自定义大头针模型
-        MMCAnnotationModel *annotationModel = [[MMCAnnotationModel alloc] init];
-        annotationModel.coordinate = self.mapView.userLocation.location.coordinate;
-        
-        annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotationModel reuseIdentifier:identifier];
-        annotationView.canShowCallout = YES;
-    }
-    return annotationView;
+}
 
+//添加大头针视图  当添加大头针时，该方法被自动调用
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    static NSString *identifier = @"MKAnnotationViewIdentifier";
+    //系统自定义的大头针模型
+    if ([annotation isKindOfClass:[MKUserLocation class]]) {
+        MKPinAnnotationView *pinAnnotationView = (MKPinAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:identifier];
+        if (!pinAnnotationView) {
+            pinAnnotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:identifier];
+            pinAnnotationView.canShowCallout = YES;
+            pinAnnotationView.pinTintColor = [UIColor colorWithRed:arc4random_uniform(256)/255.0 green:arc4random_uniform(256)/255.0 blue:arc4random_uniform(256)/255.0 alpha:1];
+            pinAnnotationView.animatesDrop = YES;
+        }
+        return pinAnnotationView;
+    }
+    //用户自定义的大头针模型
+    if ([annotation isKindOfClass:[MMCAnnotationModel class]]) {
+        MKAnnotationView *annotationView = [mapView dequeueReusableAnnotationViewWithIdentifier:identifier];
+        if (!annotationView) {
+            annotationView = [[MKAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:identifier];
+            annotationView.image = [UIImage imageNamed:@"annotation"];
+        }
+        return annotationView;
+    }
+    //不需要自定义，完全由系统处理
+    return nil;
 }
 
 //地图范围改变时调用
 - (void)mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated {
     
-}
-
-#pragma mark - touchBegin
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    //获取点击的点
-    CGPoint touchPoint = [[touches anyObject] locationInView:self.mapView];
-    //将点转化为经纬度
-    CLLocationCoordinate2D coordination = [self.mapView convertPoint:touchPoint toCoordinateFromView:self.mapView];
-    //添加大头针
-    MMCAnnotationModel *annotation = [[MMCAnnotationModel alloc] init];
-    annotation.coordinate = coordination;
-    annotation.title = @"aa";
-    annotation.subtitle = @"bb";
-    
-    [self.mapView addAnnotation:annotation];
 }
 
 #pragma mark - lazy
